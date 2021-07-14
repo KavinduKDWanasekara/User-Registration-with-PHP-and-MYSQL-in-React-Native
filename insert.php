75c26120c3ddee07132dbe9481905e11b0bf9c7c
Kavindu Wanasekara <66565953+KavinduKDWanasekara@users.noreply.github.com>
2021-07-15 05:22:41 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,25 @@
+<?php
+	$CN = mysqli_connect("localhost","root","");
+	$DB = mysqli_select_db($CN, "cst");
+	
+	$EncodedData = file_get_contents('php://input');
+	$DecodedData = json_decode($EncodedData, true);
+	
+	$RollNo = $DecodedData['RollNo'];
+	$StudentName = $DecodedData['StudentName'];
+	$Course = $DecodedData['Course'];
+	
+	$IQ = "insert into studentmaster (RollNo,StudentName,Course) values ($RollNo, '$StudentName','$Course')"; 
+	
+	$R = mysqli_query($CN, $IQ);
+	
+	if($R){
+		$Message = "Student has been registered successfully.."; 
+	}else{
+		$Message = "Server Error..Please try again later.."; 
+	}
+	
+	$Response[] = array("Message"=>$Message);
+	
+	echo json_encode($Response);
+?>
